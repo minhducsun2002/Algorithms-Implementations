682756951304d041d6ef289aae05632710d5ca45
DungWinux <ntddebugger@gmail.com>
2018-02-06 10:35:06 +0700

    Add operator, at and set

--- Matrix/matrix.hpp
+++ Matrix/matrix.hpp
@@ -22,12 +22,18 @@ class Matrix
     // Side M
     int N = 1;
     // Side N
-    __Matrix _Matrix;
 
   public:
+    __Matrix _Matrix;
     Matrix();
     Matrix(int, int, int);
     void Print();
+    int at(int m, int n) { return _Matrix[m][n]; }
+    void set(int m, int n, int val = 0) { if (m < M && n < N && m >= 0 && n >= 0) _Matrix[m][n] = val; }
+    Matrix operator+(const Matrix&);
+    Matrix operator-(const Matrix&);
+    Matrix operator*(const Matrix&);
+
 };
 Matrix::Matrix()
 {
@@ -53,4 +59,42 @@ void Matrix::Print()
     }
 }
 
+Matrix Matrix::operator+(const Matrix &param)
+{
+    Matrix temp(M, N);
+    if (N == param.N && M == param.M)
+    {
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                temp.set(i, j, this->_Matrix[i][j] + param._Matrix[i][j]);
+    }
+    return temp;
+}
+Matrix Matrix::operator-(const Matrix& param)
+{
+    Matrix temp(M, N);
+    if (N == param.N && M == param.M)
+    {
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++)
+                temp.set(i, j, this->_Matrix[i][j] - param._Matrix[i][j]);
+    }
+    return temp;
+}
+Matrix Matrix::operator*(const Matrix& param)
+{
+    Matrix temp(M, param.N);
+    if (N == param.M)
+    {
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < param.N; j++)
+            {
+                int s = 0;
+                for (int k = 0; k < N; k++)
+                    s += this->_Matrix[i][k] * param._Matrix[k][j];
+                temp.set(i, j, s);
+            }
+    }
+    return temp;
+}
 #endif

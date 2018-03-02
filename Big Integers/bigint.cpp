1b088083d4b3c5d3813f1d3f2c0bcc89f43178dc
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2018-03-02 10:27:26 +0700

    Adding addition operator
    Start implementing addition operator. Not finished yet (dependent on the subtraction operator,which isn't currently completely implemented). Untested.

--- Big Integers/bigint.cpp
+++ Big Integers/bigint.cpp
@@ -34,22 +34,23 @@ class bigint
     bool negative = false;  //default sign is positive (+)
 
     public:
-    //constructor
-    bigint();   //create instance with default value (= 0);
-    bigint(string str); //create instance with defined value
-    bigint(string str, bool sign);  //create instance with defined value and sign
-    bigint(lint num);   //create instance with given numerical value in STL integer
+    //constructor																							//==done==
+    bigint();   //create instance with default value (= 0);													//done
+    bigint(string str); //create instance with defined value												//done
+    bigint(string str, bool sign);  //create instance with defined value and sign							//done
+    bigint(lint num);   //create instance with given numerical value in STL integer							//done
                         //(long long int used to allow casting from all integers in STL's scope)
 
     //data type manipulation
-    string toString();
-    void setSign(bool negativity);
-    void setValue(string str);
-    void setValue(bigint llint);
-    bool getSign(); char getSignInChar();
-    string getValue();    
-    string getFullValue();
-    
+    string toString();																						//==done==
+    void setSign(bool negativity);																			//done									
+    void setValue(string str);																				//done							
+    void setValue(bigint llint);																			//done							
+    bool getSign(); char getSignInChar();																	//done														
+    string getValue();    																					//done
+    string getFullValue();																					//done
+	bool isNeg();																							//done
+    void operator = (bigint otherObject);																	//done
 
     //arithmetic operations
     bigint operator + (bigint addend);  //addend + addend = summands + summands = sum (= augend + addend)
@@ -67,14 +68,14 @@ class bigint
 
 //data type manipulation
 string bigint::toString()               {return ((negative ? "" : "-") + value);}
-void bigint::setSign(bool negativity)   {negative = true;}
+void bigint::setSign(bool negativity)   {negative = negativity;}
 void bigint::setValue(string str)       {value = str;}
 void bigint::setValue(bigint llint)     {setValue(llint.toString());}
 bool bigint::getSign()                  {return negative;}
 char bigint::getSignInChar()            {return (negative ? '-' : '+');}
 string bigint::getValue()               {return value;}
 string bigint::getFullValue()           {return this->toString();}
-
+void bigint::operator = (bigint otherObject) {this->value = otherObject.value; this.negative = otherObject.negative;}
 
 //constructors
 bigint::bigint()                        
@@ -138,6 +139,80 @@ bigint::bigint(lint num)
     value = to_string(num);
 }
 
+bigint bigint::operator + (bigint addend)
+{
+	bool add1_neg = this->negative, add2_neg = this->negative;
+	if (add1_neg && add2_neg)	//all negative
+	{
+		bigint tmp1 = *this; tmp1.setSign(false);
+		bigint tmp2 = addend; tmp2.setSign(false);
+		bigint result = tmp1 + tmp2; result.setSign(true);
+		return result;
+	};
+	if (add1_neg && (!add2_neg)) //add1 is negative
+	{
+		bigint tmp = *this; tmp.setSign(false);
+		return (addend - tmp);
+	};
+	if (add2_neg && (!add1_neg)) //add2 is negative
+	{
+		bigint tmp = addend; tmp.setSign(false);
+		return (*this - tmp);
+	}
+	string add1 = this->getValue(), add2 = addend.getValue();
+	
+	
+	while (add1.length() < add2.length()) add1 = "0" + add1;
+	while (add2.length() < add1.length()) add2 = "0" + add2;
+	
+	string answer = "";
+	
+	reverse(add1.begin(), add1.end());
+	reverse(add2.begin(), add2.end());
+	
+	int carry = 0;
+	for (lint i = 0 ; i <= add1.length() - 1; i++)
+	{
+		int sum = integerify(add1[i]) + integerify(add2[i]) + carry;
+		carry = 0;
+		int push = sum % 10; char push_ = push + 48;
+		carry = push / 10;
+		answer.push_back(push_);
+	};
+	if (carry != 0) answer.push_back('1');
+	
+	reverse(answer.begin(), answer.end());
+	bigint returnValue (answer, false);
+	return returnValue;
+}
+
+
+bigint bigint::operator - (bigint subtrahend)
+{
+	bool add1_neg = this->getSign(), add2_neg = subtrahend.getSign();
+	if (add1_neg && add2_neg)	//all negative
+	{
+		bigint tmp1 (this->getValue(), !add1_neg);
+		bigint tmp2 (subtrahend.getValue(), !add2_neg);;
+		return (tmp2 - tmp1);
+	};
+	if (add1_neg && !add2_neg) 	//add1 is negative
+	{
+		bigint tmp1 (this->getValue(), false);
+		return (subtrahend - tmp1);
+	};
+	if (!add1_neg && add2_neg)
+	{
+		bigint tmp1 (subtrahend->getValue(), false);
+		return (*this - tmp1);
+	}
+	
+	
+	string add1 = this->getValue(), add2 = subtrahend.getValue();
+	
+}
+
+
 
 /*
 int main()
@@ -148,22 +223,16 @@ int main()
     llint _instance1 (str);
     llint _emptyInstance;
     cout << "Results: " << endl;
-
     //for each function, <cout> a line
-
     //toString and all constructors are tested
     cout << _instance1.bigint::toString() << endl;
     cout << "[Default value:] " << _emptyInstance.toString() << endl;
-
     llint _instance2 (str, true); cout << "[Force negative:] " << _instance2.toString() << endl;
     cout << "[Instance2.sign:] " << _instance2.getSign() << endl;
-
     llint _instance3 (_instance2.toString(), false); cout << "[Force positive (after negating above): ] " << _instance3.toString() << endl;
     cout << "[Instance3.setSign(neg):] "; _instance3.setSign(true); cout << _instance3.getSign() << endl;
-
     cout << "Please enter a number (<long long int> range): "; lint _tmpLLInt; cin >> _tmpLLInt;
     llint _instance4 (_tmpLLInt); cout << "[Converted value:] " << _instance4.toString() << endl;
     cout << "[getFullValue:] "; _instance3.setSign(false); cout << _instance3.getFullValue() << endl;
-
 }
 */

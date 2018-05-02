8bb829aaaf1d1dca3fbf3c28da44d91817bdfbaf
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2018-05-02 12:44:11 +0700

    Add wrapping function
    Using patience sort method

--- Subsequences/LongestIncreasingSubsequences.cpp
+++ Subsequences/LongestIncreasingSubsequences.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 typedef long long int llint;
 const long long int LLINT_MAX = LLONG_MAX;
 const long long int LLINT_MIN = LLONG_MIN;
+string padding (4, ' ');
 
 vector <vector <llint> > sort_to_piles (vector <llint> arr)
 {	
@@ -38,6 +38,20 @@ vector <vector <llint> > sort_to_piles (vector <llint> arr)
 	*/
 }
 
+vector <vector <llint> > lis (vector <llint> arr)
+{
+	vector <vector <llint> > piles = sort_to_piles(arr);
+	llint max = LLONG_MIN;
+	for (llint i = 0 ; i <= piles.size() - 1; i++)
+	if (piles[i].size() > max)
+	{
+		max = piles[i].size();
+	}
+	vector <vector <llint> > ret;
+	for (llint i = 0 ; i <= piles.size() - 1; i++) if (piles[i].size() == max) ret.push_back(piles[i]);
+	return ret;
+}
+
 int main()
 {
 	cout << "Enter number of elements: ";
@@ -48,6 +62,9 @@ int main()
 	{
 		cout << "Element number " << i + 1 << " : "; cin >> arr[i];
 	};
+	
+	//old code
+	/*
 	vector <vector <llint> > piles = sort_to_piles(arr);
 	llint max = LLONG_MIN, max_id = 0;
 	for (llint i = 0 ; i <= piles.size() - 1; i++)
@@ -58,4 +75,15 @@ int main()
 	}
 	cout << "Longest increasing subsequence found:\n\tLength: " << max << "\n\tSubsequence: ";
 	for (llint i = 0 ; i <= piles[max_id].size() - 1 ; i++) cout << piles[max_id][i] << ' ';
+	*/
+	
+	//demonstration code	
+	vector <vector <llint> > lisResult = lis(arr);
+	cout << "Longest increasing subsequence" << ((lisResult.size() > 1) ? "s" : "") << " found:\n" << padding << "Length: " << lisResult[0].size() << "\n" << padding << "Subsequence" << ((lisResult.size() > 1) ? "s" : "") << ":\n";
+	for (llint i = 0 ; i <= lisResult.size() - 1; i++)
+	{
+		cout << padding << i + 1 << ")\t"; 
+		for (llint n = 0 ; n <= lisResult[i].size() - 1 ; n++) cout << lisResult[i][n] << " ";
+		cout << endl;
+	}
 }

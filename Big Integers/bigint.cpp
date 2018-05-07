f6539d0d594671f06a0a7a67d272b7d43616f432
minhducsun2002 <32769878+minhducsun2002@users.noreply.github.com>
2018-05-07 10:23:48 +0700

    Some clean-up and shorten some parts

--- Big Integers/bigint.cpp
+++ Big Integers/bigint.cpp
@@ -32,7 +32,10 @@ class bigint
     private:
     string value = "0" ;    //default value is initialized to 0
     bool negative = false;  //default sign is positive (+)
-
+	pair <string, string> standardify(string str1, string str2);	//padding the operands
+	string add(string str1, string str2);	//internal addition logic
+	string sub(string str1, string str2);	//internal subtraction logic
+	
     public:
     //constructor																							//==done==
     bigint();   //create instance with default value (= 0);													//done
@@ -78,191 +81,42 @@ string bigint::getFullValue()           {return this->toString();}
 void bigint::operator = (bigint otherObject) {this->value = otherObject.value; this.negative = otherObject.negative;}
 
 //constructors
-bigint::bigint()                        
-{
-    value = "0";
-}
+bigint::bigint()                       	{value = "0";}
 
 bigint::bigint(string str)
 {
-    if (str.empty())
-    {
-        value = "0";
-        return;
-    }
+    if (str.empty()) value = "0";
     else
     {
-        if (str[0] == '-')
+        if (str.front() == '-')
         {
-            str.erase(0,1);
-            negative = true;
-            if (str.empty()) value = "0"; else value = str;
-            return;
+            str.erase(str.begin()); negative = true; 
+			value = (str.empty()) ? "0" : str;
         }
-        else
-        {
-            value = str; return;
-        }
-    }
+        else value = str;
+    };
 }
 
-bigint::bigint(string str, bool sign)       //warning: WIP (work in progress), may make conflicts when string's sign and the boolean value is different
+bigint::bigint(string str, bool sign)       //warning: WIP (work in progress), may conflicts when string's sign and the boolean value is different
 {
     //well, just override the string's sign with the given sign
     if (str.empty())
     {
-        value = "0";
-        negative = sign;
-        return;
+        value = "0"; negative = sign;
     }
     else
     {
-        if (str[0] == '-')
+        if (str.front() == '-')
         {
-            str.erase(0,1);
-            negative = true;
-            if (str.empty()) value = "0"; else value = str;
-            negative = sign;
-            return;
+        	str.erase(str.begin()); negative = sign; 
+            value = (str.empty()) ? "0" : str;
         }
         else
         {
-            value = str;
-            negative = sign;
-            return;
+            value = str; negative = sign;
         }
     }
 }
 
-bigint::bigint(lint num)
-{
-    value = to_string(num);
-}
+bigint::bigint(lint num)				{value = to_string(num);}
 
-bigint bigint::operator + (bigint addend)
-{
-	bool add1_neg = this->negative, add2_neg = this->negative;
-	if (add1_neg && add2_neg)	//all negative
-	{
-		bigint tmp1 = *this; tmp1.setSign(false);
-		bigint tmp2 = addend; tmp2.setSign(false);
-		bigint result = tmp1 + tmp2; result.setSign(true);
-		return result;
-	};
-	if (add1_neg && (!add2_neg)) //add1 is negative
-	{
-		bigint tmp = *this; tmp.setSign(false);
-		return (addend - tmp);
-	};
-	if (add2_neg && (!add1_neg)) //add2 is negative
-	{
-		bigint tmp = addend; tmp.setSign(false);
-		return (*this - tmp);
-	}
-	string add1 = this->getValue(), add2 = addend.getValue();
-	
-	//padding
-	while (add1.length() < add2.length()) add1 = "0" + add1;
-	while (add2.length() < add1.length()) add2 = "0" + add2;
-	
-
-
-	string answer = "";
-	
-	reverse(add1.begin(), add1.end());
-	reverse(add2.begin(), add2.end());
-	
-	int carry = 0;
-	for (lint i = 0 ; i <= add1.length() - 1; i++)
-	{
-		int sum = integerify(add1[i]) + integerify(add2[i]) + carry;
-		carry = 0;
-		int push = sum % 10; char push_ = push + 48;
-		carry = push / 10;
-		answer.push_back(push_);
-	};
-	if (carry != 0) answer.push_back('1');
-	
-	reverse(answer.begin(), answer.end());
-	bigint returnValue (answer, false);
-	return returnValue;
-}
-
-
-bigint bigint::operator - (bigint subtrahend)
-{
-	bool add1_neg = this->getSign(), add2_neg = subtrahend.getSign();
-	if (add1_neg && add2_neg)	//all negative
-	{
-		bigint tmp1 (this->getValue(), !add1_neg);
-		bigint tmp2 (subtrahend.getValue(), !add2_neg);;
-		return (tmp2 - tmp1);
-	};
-	if (add1_neg && !add2_neg) 	//add1 is negative
-	{
-		bigint tmp1 (this->getValue(), false);
-		return (subtrahend - tmp1);
-	};
-	if (!add1_neg && add2_neg)
-	{
-		bigint tmp1 (subtrahend->getValue(), false);
-		return (*this - tmp1);
-	}
-	
-	string add1 = this->getValue(), add2 = subtrahend.getValue();
-	
-
-    //padding
-	while (add1.length() < add2.length()) add1 = "0" + add1;
-	while (add2.length() < add1.length()) add2 = "0" + add2;
-
-
-    bool neg = false;
-    if (add1 < add2)
-    {
-         add1.swap(add2); neg = true;
-    }
-
-    
-    int remsub = 0;     //carry
-    string answer = "";
-    for (lint i = add1.length() - 1 ; i >= 0 ; i--)
-    {
-        int current = (add1[i] - '0') - (add2[i] - '0') - remsub;   //calculating
-        remsub = 0; //clear value
-        if (curr < 0)
-        {
-            remsub = 1; //raise if have to borrow
-            curr += 10;
-        };
-        answer.append(1, ((curr % 10) + '0'))
-    }
-    std::reverse(answer.begin(), answer.end());
-    bigint tmp (answer, neg);
-    return tmp;
-}
-
-
-
-/*
-int main()
-{
-    cout << "Please enter a number: ";
-    string str; cin >> str;
-    typedef bigint llint;
-    llint _instance1 (str);
-    llint _emptyInstance;
-    cout << "Results: " << endl;
-    //for each function, <cout> a line
-    //toString and all constructors are tested
-    cout << _instance1.bigint::toString() << endl;
-    cout << "[Default value:] " << _emptyInstance.toString() << endl;
-    llint _instance2 (str, true); cout << "[Force negative:] " << _instance2.toString() << endl;
-    cout << "[Instance2.sign:] " << _instance2.getSign() << endl;
-    llint _instance3 (_instance2.toString(), false); cout << "[Force positive (after negating above): ] " << _instance3.toString() << endl;
-    cout << "[Instance3.setSign(neg):] "; _instance3.setSign(true); cout << _instance3.getSign() << endl;
-    cout << "Please enter a number (<long long int> range): "; lint _tmpLLInt; cin >> _tmpLLInt;
-    llint _instance4 (_tmpLLInt); cout << "[Converted value:] " << _instance4.toString() << endl;
-    cout << "[getFullValue:] "; _instance3.setSign(false); cout << _instance3.getFullValue() << endl;
-}
-*/

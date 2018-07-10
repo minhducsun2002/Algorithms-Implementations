31598ae726f84c34a6cb4a59196529110998b600
Dung Winux <ntddebugger@gmail.com>
2018-07-10 14:37:11 +0700

    Refactor dijkstra

--- Graph/Shortest Path/Adjacency List/dijkstra.cpp
+++ Graph/Shortest Path/Adjacency List/dijkstra.cpp
@@ -1,111 +1,30 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <string>
+#include "dijkstra.hpp"
 
-typedef long long int llint;
-typedef size_t ullint;
-
-// Dijkstra's algorithm to find shortest path between two vertices in a graph,
-// using adjacency list representation
-
-const long long int MAX = 1e9 + 7;
-
-// Note that the index of each vertex counts from 0.
-
-vector <bool> traversed;
-// log if a specific vertex has been checked
-// preventing infinite loop
-
-struct edge
+int main()
 {
-	llint weight, targetVertex;
-	// for each vertex
-	// its adjacency list entry will contain informations about its neighbour(s)
-	// including the weight of the edge connecting them
-};
-
-vector <vector <edge> > adjacencyList;
-
-vector <llint> dijkstra (llint source, llint target)
-{
-	llint verticesCount = adjacencyList.size();
-
-	vector <llint> distance (adjacencyList.size(), MAX);
-	// the distance between a vertex and itself == 0;
-	traversed[source] = true;
-	distance[source] = 0;
-	
-	vector <llint> backtrace (adjacencyList.size(), MAX);
-	// keep the shortest path
-
-	for (llint currentVertex = 0; currentVertex <= verticesCount - 1 ; currentVertex++)
-	{
-		// for each vertex
-		for (edge i : adjacencyList[currentVertex])
-		{
-			// if current neighbour is unvisited
-			if (!traversed[i.targetVertex])
-			{
-				// then visit it!
-				traversed[i.targetVertex] = true;
-				// and also calculate the distance from current node
-				distance[i.targetVertex] = distance[currentVertex] + i.weight;
-				// overwriting traces
-				backtrace[i.targetVertex] = currentVertex;
-			}
-			// and if visited
-			else
-			{
-				// if path isn't optimal
-				if (distance[i.targetVertex] > distance[currentVertex] + i.weight) 
-				{
-			        // then make it optimal!
-					distance[i.targetVertex] = distance[currentVertex] + i.weight;
-				    // and don't forget overwriting old traces
-					backtrace[i.targetVertex] = currentVertex;
-				}
-			};
-		};
-	};
-	
-	vector <llint> path;
-    // trace back to source
-    llint current_head = target;
-    while (current_head != source)
-    {
-        path.push_back(current_head);
-        current_head = backtrace[current_head];
-    }
-	// in case
-	// the path is wanted in order source -> destination then uncomment the line below
-	// reverse (path.begin(), path.end()); 
-	return path;
-	// in case the shortest distance to each node is wanted (instead of shortest path),
-	// then
-	// ```
-	// return distance;
-	// ```
-	// instead.
-}
-
-main()
-{
-	llint verticesCount; cin >> verticesCount;
+	llint verticesCount;
+	std::cin >> verticesCount;
 	// prepare traversal logging and adjacency list
 	traversed.resize(verticesCount);
 	adjacencyList.resize(verticesCount);
 
-	for (llint i = 0 ; i <= verticesCount - 1 ; i++)
+	for (llint i = 0; i <= verticesCount - 1; i++)
 	{
-		llint neighbour_count; cin >> neighbour_count;
+		llint neighbour_count;
+		std::cin >> neighbour_count;
 		while (neighbour_count--)
 		{
 			// edge between vertex[i] and vertex[push.targetVertex] with weight = push.weight
-			edge push; cin >> push.targetVertex >> push.weight;
+			edge push;
+			std::cin >> push.targetVertex >> push.weight;
 			adjacencyList[i].push_back(push);
 		};
 	};
-	vector <llint> out = dijkstra(0, 4);
-	for (llint i : out) cout << (i != MAX ? to_string(i) : string ("MAX")) << " ";
+	std::vector<llint> out = dijkstra(0, 4);
+	for (llint i : out)
+		std::cout << (i != MAX ? std::to_string(i) : std::string("MAX")) << " ";
 }
 
 /*

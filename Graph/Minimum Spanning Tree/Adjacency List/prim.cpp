637116175e26418c06a7fd958fb441847b3f2931
minhducsun2002 <minhducsun2002@gmail.com>
2018-07-17 14:45:18 +0700

    Add Prim's Minimum Spanning Tree algorithm using adjacency list
    Now can print the MST (parent of each vertex).

--- Graph/Minimum Spanning Tree/Adjacency List/prim.cpp
+++ Graph/Minimum Spanning Tree/Adjacency List/prim.cpp
@@ -12,46 +12,78 @@ const long long int MAX = 1e9 + 7;
 // Note that the index of each vertex counts from 0.
 
 typedef pair <llint, llint> edge;
+// edge.first == weight, edge.second == target_vertex
+
+typedef pair <llint, edge> extended_edge;
+// extended.first == weight
+// 			second.first == source
+// 			second.destination == destination
 
 vector <vector <edge > > adjacencyList;
 
-// pair.first == weight, pair.second == target_vertex
+edge make_edge_from_to(llint source, llint destination)
+{
+	return make_pair(source, destination)
+	;
+}
 
-llint prim(llint source, vector <vector <edge> > adjacencyList)
+auto prim(llint source, vector <vector <edge> > adjacencyList)
 {
 	vector <bool> included (adjacencyList.size(), false);
 	// tracking whether vertex included
 
-	multiset <edge> remaining_edges;
-	remaining_edges.insert(make_pair(0, source));
+	multiset <extended_edge> remaining_edges;
+	// track unprocessed edges
 
-	llint MST_size = 0;
+	remaining_edges.insert(make_pair(0, make_edge_from_to(source, source)));
+	// the distance from source -> source == 0
+
+	vector <llint> parent (adjacencyList.size());
+
+	// llint MST_size = 0;	// *
 
 	while (!remaining_edges.empty())
 	{
-		pair <llint, llint> nearest = *remaining_edges.begin();
+		auto	// i.e. the shortest unprocessed extended_edge
+		nearest = *remaining_edges.begin();
+
+		// and then erase it since processed
 		remaining_edges.erase(remaining_edges.begin());
 
-		if (included[nearest.second])
+		if (included[nearest.second.second])
 			// if already considered
+			// i.e. if nearest.destination is included in the MST
 		{
 			continue;
-			// then skip that vertex
+			// then skip that vertex!
 		};
 
-		MST_size += nearest.first;
+		// MST_size += nearest.first;	// *
 
-		included[nearest.second] = true;
+
+		parent[nearest.second.second]
+		= nearest.second.first;
+
+		included[nearest.second.second] = true;
 		// include this vertex (i.e. use the current edge in the MST)
 
-		for (pair <llint, llint> a: adjacencyList[nearest.second])
+		for (pair <llint, llint> a: adjacencyList[nearest.second.second])
 			// for each edge from this vertex
+			// i.e. the "just included" vertex
 		{
 			if (!included[a.second])
-				remaining_edges.insert(a);
+				// if vertex (in adjacencyList) isn't included
+				remaining_edges.insert(make_pair(a.first, make_edge_from_to(nearest.second.second, a.second)));
 		}
 	}
-	return MST_size;
+
+	// return MST_size;	// *
+
+	// lines marked with a star are used to compute the size of the MST.
+	// Uncomment to calculate. Alter the return object correspondingly.
+
+
+	return parent;
 }
 
 main()
@@ -71,5 +103,36 @@ main()
 			adjacencyList[i].push_back(a);
 		};
 	};
-	cout << prim(0, adjacencyList);
+	
+
+	vector <llint> out = prim (0, adjacencyList);
+	llint out_count = 0;
+	for (auto &I : out) cout << "Parent of " << out_count++ << " is " << I << endl;
 }
+
+/*
+Input specification:
+number_of_vertex
+number_of_edges edge1_other_vertex edge1_weight edge2_other_vertex edge2_weight ...
+...
+
+Output specification:
+Parents of each vertex in the MST.
+The MST's root has itself as its parent.
+
+Test:
+{input}
+5
+4 1 4 3 6 4 6 2 4
+1 2 2 
+3 1 2 0 4 3 8
+3 0 6 2 8 4 9
+2 0 6 3 9
+
+{output}
+Parent of 0 is 0
+Parent of 1 is 0
+Parent of 2 is 1
+Parent of 3 is 0
+Parent of 4 is 0
+*/

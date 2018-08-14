6b56fae946dba73988e90a7608c3918ce87e83f7
Minh Duc Vo <32769878+minhducsun2002@users.noreply.github.com>
2018-08-14 21:57:34 +0700

    Optimize complexity of Manacher's algorithm

--- String/Palindromic Substring/LongestPalindromicSubstring_Manacher.cpp
+++ String/Palindromic Substring/LongestPalindromicSubstring_Manacher.cpp
@@ -12,18 +12,26 @@ std::string manacher(std::string str)
 {
     std::string compute_table_ref (1, delimiter);
     for (char i : str) compute_table_ref += std::string (1, i) + std::string(1, delimiter);
-    std::vector <size_t> compute_table (compute_table_ref.size());
+    std::vector <std::size_t> compute_table (compute_table_ref.size());
+    auto& table = compute_table;
+    auto& table_ref = compute_table_ref;
+    
+    // computation logic
+    std::size_t right = 0, center = 0;
     for (std::size_t i = 0 ; i <= compute_table_ref.size() - 1; i++)
     {
-        std::size_t length = 0;
-        for (std::size_t n = 1 ; ; n++)
+        table[i] = (right > i) ? min (right - i, table[(center << 1) - i]) : 0;
+        while (table_ref[i - table[i] - 1]
+        	== table_ref[i + table[i] + 1]) table[i]++;
+        if (i + table[i] > right)
         {
-            if (out_of_bounds(i + n, compute_table_ref.size() - 1) || out_of_bounds(i - n, compute_table_ref.size() - 1)) break;
-            if (compute_table_ref[i + n] != compute_table_ref[i - n]) break; else length++;
+        	right = i + table[i];
+        	center = i;
         }
-        compute_table[i] = length;
     };
-    std::vector <size_t>::iterator central = std::max_element(compute_table.begin(), compute_table.end());
+    
+    // Get maximum length palindrome and parse into proper string
+    std::vector <std::size_t>::iterator central = std::max_element(compute_table.begin(), compute_table.end());
     std::size_t it = std::distance(compute_table.begin(), central);
     std::string raw_out = compute_table_ref.substr(it, *central);
     std::string out = ""; for (char i : raw_out) if (i != delimiter) out.push_back(i);
